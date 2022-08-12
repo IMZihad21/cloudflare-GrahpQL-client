378b519d57bc9c9ef85d82b5d6eb383f68bd4b5f
ZèD <imzihad@gmail.com>
2022-08-12 22:19:59 +0000

    gql: established connection to gql backend
    Signed-off-by: GitHub <noreply@github.com>

---
+++ src/configs/queries.ts
@@ -0,0 +1,19 @@
+import { gql } from "@apollo/client";
+
+export const GET_ALL_OPEN_USERS = gql`
+  query RootQueryType {
+    getAllOpenUsers {
+      name
+      email
+    }
+  }
+`;
+
+export const GET_ALL_USERS = gql`
+  query RootQueryType {
+    getAllUsers {
+      name
+      email
+    }
+  }
+`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
-import { Box, Typography } from '@mui/material'
+import { useQuery } from '@apollo/client';
+import { Box, Typography } from '@mui/material';
+import { GET_ALL_OPEN_USERS } from '../configs/queries';
 
 const Home = () => {
+    const { loading, error, data } = useQuery(GET_ALL_OPEN_USERS);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
     return (
         <Box>
-            <Typography>Material UI</Typography>
+            <Typography>
+                {JSON.stringify(data)}
+            </Typography>
         </Box>
     )
 }
